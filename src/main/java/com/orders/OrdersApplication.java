a9635dde54f97846b005e9f6c9485040faa6031a
ArchitB17976 <44702605+ArchitB17976@users.noreply.github.com>
2022-08-18 02:36:11 +0800

    Finalising app properties and main app

--- src/main/java/com/orders/OrdersApplication.java
+++ src/main/java/com/orders/OrdersApplication.java
@@ -2,12 +2,24 @@ package com.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrdersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
 	}
 
 }
